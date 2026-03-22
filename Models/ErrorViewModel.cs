35054898f30dda883873c10fcbf4f52b2070e62f
Dalio Miranda <2992817010101@ingenieria.usac.edu.gt>
2026-03-22 16:17:53 -0600

    Release 1
     Estructura base del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaDrones.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
